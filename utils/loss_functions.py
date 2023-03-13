10cf7fce84e5a508acf1bb967a7c0b0b610f0da7
Oliver Lemke <lemke.oliver@icloloud.com>
2023-03-13 15:56:53 +0100

    Added ShiftInvariantLoss

---
+++ utils/loss_functions.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+
+
+class ShiftInvariantLoss(nn.Module):
+    """
+    https://proceedings.neurips.cc/paper/2014/file/7bccfde7714a1ebadf06c5f4cea752c1-Paper.pdf
+    """
+
+    def __init__(self, lam=.5):
+        super(ShiftInvariantLoss, self).__init__()
+        self.lam = lam
+
+    def forward(self, y_pred, y_true):
+        n = y_true.size()[-1] * y_true.size()[-2]
+        diff = y_pred - y_true
+
+        left_term = torch.mean(diff ** 2)
+        right_term = torch.sum(diff, dim=[1, 2]) ** 2
+        loss_per_item = left_term - (self.lam / (n ** 2)) * right_term
+        return torch.mean(loss_per_item)
+
+
+class ReverseHuberLoss(nn.Module):
+    def __init__(self):
+        super(ReverseHuberLoss, self).__init__()
+
+    def forward(self, y_pred, y_true):
+        # TODO
+        raise NotImplementedError()
+
+
+def test():
+    # create an example 3D tensor with predicted and true values
+    y_pred = torch.tensor([
+        [[1.2, 2.4], [3.6, 4.8]],
+        [[5.1, 6.2], [7.3, 8.4]],
+        [[9.5, 10.6], [11.7, 12.8]]
+    ])
+    y_true = torch.tensor([
+        [[1.0, 2.0], [3.0, 4.0]],
+        [[5.0, 6.0], [7.0, 8.0]],
+        [[9.0, 10.0], [11.0, 12.0]]
+    ])
+    print(y_pred.size())
+
+    # instantiate your custom loss function
+    criterion = ShiftInvariantLoss()
+
+    # compute the loss between y_pred and y_true
+    loss = criterion(y_pred, y_true)
+
+    # print the loss value
+    print("Loss:", loss.item())
+
+
+if __name__ == '__main__':
+    test()
