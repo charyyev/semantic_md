4aa35a21a0f3fba2ed34d497ddb8ac31f4e617e4
Ankita Ghosh <ankitaghosh0907@gmail.com>
2023-03-21 23:48:17 +0100

    Update eval_metrics.py

--- utils/eval_metrics.py
+++ utils/eval_metrics.py
@@ -11,11 +11,13 @@ def depth_metrics(pred, target):
     delta2 = torch.sum(thresh < 1.25 ** 2).float() / len(thresh)
     delta3 = torch.sum(thresh < 1.25 ** 3).float() / len(thresh)
 
-    # mean relative error, root mean squared error, log10 error
+    # mean absolute relative error, mean squared relative error, root mean squared error, rsme log, log10 error
     diff = pred - target
     abs_rel = torch.mean(torch.abs(diff) / target)
+    sq_rel = torch.mean(torch.pow(diff, 2) / target)
     rmse = torch.sqrt(torch.mean(torch.pow(diff, 2)))
+    rmse_log = torch.sqrt(torch.mean(torch.pow(torch.log(pred) - torch.log(target), 2)))
     log10 = torch.mean(torch.abs(torch.log10(pred) - torch.log10(target)))
 
     return {'delta1': delta1.item(), 'delta2': delta2.item(), 'delta3': delta3.item(),
-            'abs_rel': abs_rel.item(), 'rmse': rmse.item(), 'log10':log10.item()}
+            'abs_rel': abs_rel.item(), 'sq_rel': sq_rel.item(), 'rmse': rmse.item(), 'rmse_log': rmse_log.item(), 'log10':log10.item()}
