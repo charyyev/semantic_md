295261c925b16adcf300dbcee22094b4108fbd2b
Oliver Lemke <lemke.oliver@icloloud.com>
2023-04-10 22:31:23 +0200

    Refactored code so that entire model architecture (including nr classes) is done in model_factory.py and adjacent modules in the models folder

--- models/model_factory.py
+++ models/model_factory.py
@@ -6,6 +6,7 @@ import torch
 import torchvision
 
 from models.unet import Unet
+from models.specialized_networks import semantic_convolution, onehot, concat, border, model_utils, simplified_onehot
 
 
 # https://smp.readthedocs.io/en/latest/models.html#unet
@@ -20,27 +21,26 @@ class ModelFactory:
         (3) A name under which to find pretrained weights
         (4) A model type description which specifies which the type of model. Used when extending the #channels
         """
-        self.models = dict(
+        self.basic_models = dict(
             # unet=(Unet, {}, "unet"),
             uresnet34=(
                 smp.Unet, {"encoder_name": "tu-resnet34", "encoder_weights": None, "activation": "sigmoid"},
-                "resnet34", "timm_smp"),
+                "resnet34", "timm_smp_res"),
             uresnet50=(
                 smp.Unet, {"encoder_name": "tu-resnet50", "encoder_weights": None, "activation": "sigmoid"},
-                "resnet50", "timm_smp"),
+                "resnet50", "timm_smp_res"),
             uefficientnet_b2=(
                 smp.Unet, {"encoder_name": "tu-efficientnet_b2", "encoder_weights": None, "activation": "sigmoid"},
-                "efficientnet_b2", "timm_smp"),
+                "efficientnet_b2", "timm_smp_eff"),
             uefficientnet_b3=(
                 smp.Unet, {"encoder_name": "tu-efficientnet_b3", "encoder_weights": None, "activation": "sigmoid"},
-                "efficientnet_b3", "timm_smp"),
+                "efficientnet_b3", "timm_smp_eff"),
             uefficientnet_b4=(
                 smp.Unet, {"encoder_name": "tu-efficientnet_b4", "encoder_weights": None, "activation": "sigmoid"},
-                "efficientnet_b4", "timm_smp")
+                "efficientnet_b4", "timm_smp_eff")
         )
 
-    def get_model(self, model_type: str, pretrained_weights_path: str, in_channels: int = 3,
-                  semantic_convolution=False):
+    def get_model(self, model_type: str, pretrained_weights_path: str, config, in_channels: int = 3):
         """
         Instantiates model from available pool
         :param model_type: can be one of ['unet', 'uresnet34', 'uresnet50', 'uefficientnet_b2', 'uefficientnet_b3',
@@ -56,111 +56,46 @@ class ModelFactory:
         if model_type == "unet":
             model = Unet(in_c=in_channels)
             transforms = {"mean": (0, 0, 0), "std": (1, 1, 1)}
-        else:
-            model_func, kwargs, name, type_desc = self.models[model_type]
+            type_desc = "unet"
+        elif model_type in list(self.basic_models.keys()):
+            model_func, kwargs, name, type_desc = self.basic_models[model_type]
 
             # if it is a smp model with timm encoder, it can only handle pretrained weights with 3 input channels
             # therefore we have to manually extend the input_channels via the below function
-            if type_desc == "timm_smp" and in_channels > 3:
+            if type_desc in ["timm_smp_res", "timm_smp_eff"] and in_channels > 3:
                 model = model_func(**kwargs, in_channels=3, classes=1)
             else:
                 model = model_func(**kwargs, in_channels=in_channels, classes=1)
 
             metadata_path = os.path.join(pretrained_weights_path, name, "weights_object.pickle")
             with open(metadata_path, "rb") as file:
-                pickled = pickle.load(file)
-                transforms = pickled
-            # use pretrained only with image
-            if in_channels == 3:
-                weights_path = os.path.join(pretrained_weights_path, name, "weights.pth")
-                weights_dict = torch.load(weights_path)
-                # we have additional weights in the saved weights (for classification and stuff), so we do strict = False
-                model.encoder.model.load_state_dict(weights_dict, strict=False)
+                transforms = pickle.load(file)
+            weights_path = os.path.join(pretrained_weights_path, name, "weights.pth")
+            weights_dict = torch.load(weights_path)
+            # we have additional weights in the saved weights (for classification and stuff), so we do strict = False
+            model.encoder.model.load_state_dict(weights_dict, strict=False)
 
             # if it is a smp model with timm encoder, it can only handle pretrained weights with 3 input channels
             # therefore we have to manually extend the input_channels via the below function
-            if type_desc == "timm_smp" and in_channels > 3:
-                # pretrained_conv1 = model.encoder.model.conv1
-                pretrained_conv1 = model.encoder.model.conv_stem
+            # rarely used practice
+            if type_desc in ["timm_smp_res", "timm_smp_eff"] and in_channels > 3:
+                get_func, set_func = model_utils.get_set_conv1_functions(type_desc)
                 add_out_channels = in_channels - 3
-                model = _extend_first_convolution(model, pretrained_conv1, _resnet_set_conv1_func, add_out_channels)
+                model = model_utils.extend_first_convolution(model, add_out_channels, get_func, set_func)
+        else:
+            raise ValueError(f"Unknown model_type {model_type}")
 
-        if semantic_convolution:
-            model = SemanticConvolutionModel(model, semantic_out_channels=in_channels)
+        get_func, set_func = model_utils.get_set_conv1_functions(type_desc)
+        data_flags = config["data_flags"]
+        if data_flags["semantic_convolution"]:
+            model = semantic_convolution.SemanticConvolutionModel(model, in_channels, get_func, set_func)
+        elif data_flags["onehot"]:
+            model = onehot.OneHotModel(model, data_flags["seg_classes"], get_func, set_func)
+        elif data_flags["concat"]:
+            model = concat.ConcatModel(model, get_func, set_func)
+        elif data_flags["border"]:
+            model = border.BorderModel(model, get_func, set_func)
+        elif data_flags["simplified_onehot"]:
+            model = simplified_onehot.SimplifiedOneHotModel(model, get_func, set_func)
 
         return model, transforms
-
-
-def _extend_first_convolution(pretrained_model, pretrained_conv1, set_conv1_func, additional_out_channels):
-    """
-    This function extends to initial convolution of a model to more input channels, while keeping the pretrained weights
-    for the non-new parts of the convolution.
-    :param pretrained_model: The pretrained model to be extended
-    :param pretrained_conv1: The first convolution of the model to be extended
-    :param set_conv1_func: A function that allows the setting of said first convolution via
-    set_conv1_func(pretrained_model, new_conv1). Necessary as accessing the first conv is different for every model
-    :param additional_out_channels:  Number of channels by which the convolution should be extended
-    :return:
-    """
-    # Take the weights of the first convolutional layer of the pre-trained model
-    pretrained_conv1 = pretrained_conv1
-
-    # Initialize only the weights of the first 3 channels with the pre-trained weights
-    # Create exact same convolution
-    in_channels = pretrained_conv1.in_channels + additional_out_channels
-    own_conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=pretrained_conv1.out_channels,
-                                kernel_size=pretrained_conv1.kernel_size, stride=pretrained_conv1.stride,
-                                padding=pretrained_conv1.padding, bias=pretrained_conv1.bias)
-    # Create a new convolution where part of the weights are pretrained
-    new_weights = own_conv1.weight.clone()
-    new_weights[:, :pretrained_conv1.in_channels, :, :] = \
-        pretrained_conv1.weight[:, :pretrained_conv1.in_channels, :, :]
-    own_conv1.weight = torch.nn.Parameter(new_weights)
-
-    # Implant new convolution with partially pretrained weights into model
-    pretrained_model = set_conv1_func(pretrained_model, own_conv1)
-    return pretrained_model
-
-
-def _resnet_set_conv1_func(pretrained_model, conv1):
-    pretrained_model.encoder.model.conv1 = conv1
-    return pretrained_model
-
-
-def _efficientnet_set_conv1_func(pretrained_model, conv1):
-    pretrained_model.encoder.model.conv_stem = conv1
-    return pretrained_model
-
-
-class SemanticConvolutionModel(torch.nn.Module):
-    def __init__(self, pretrained_model, semantic_out_channels):
-        super(SemanticConvolutionModel, self).__init__()
-        self.pre_trained_model = pretrained_model
-        # pretrained_conv1 = self.pre_trained_model.encoder.model.conv1
-        pretrained_conv1 = self.pre_trained_model.encoder.model.conv_stem
-        self.model = _extend_first_convolution(pretrained_model, pretrained_conv1, _efficientnet_set_conv1_func,
-                                               semantic_out_channels)
-
-        mbconv_config = torchvision.models.efficientnet.MBConvConfig(
-            expand_ratio=6, kernel=3, stride=1, input_channels=1, out_channels=semantic_out_channels, num_layers=1,
-            width_mult=1.4, depth_mult=1.8
-        )
-        self.convolution = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding=1, bias=False),
-            torch.nn.BatchNorm2d(num_features=8),
-            torchvision.models.efficientnet.MBConv(mbconv_config, 0.2, torch.nn.BatchNorm2d),
-            torch.nn.Conv2d(in_channels=8, out_channels=semantic_out_channels, kernel_size=1, stride=1, bias=False),
-            torch.nn.BatchNorm2d(num_features=semantic_out_channels),
-            torch.nn.Sigmoid()
-        )
-        # Alternative convolution
-        # self.convolution = torch.nn.Sequential(
-        #     torch.nn.Conv2d(in_channels=1, out_channels=3, kernel_size=3, stride=1, padding=1),
-        #     torch.nn.Sigmoid()
-        # )
-
-    def forward(self, image, semantic_map):
-        semantic_map_conv = self.convolution(semantic_map)
-        cat = torch.cat([image, semantic_map_conv], dim=1)
-        out = self.model(cat)
-        return out
---
+++ models/specialized_networks/onehot.py
@@ -0,0 +1,13 @@
+from torch import nn
+
+from models.specialized_networks import model_utils
+
+
+class OneHotModel(nn.Module):
+    def __init__(self, pretrained_model, nr_classes, get_func, set_func):
+        super(OneHotModel, self).__init__()
+        self.model = model_utils.extend_first_convolution(pretrained_model, nr_classes, get_func, set_func)
+
+    def forward(self, image_semantic_onehot):
+        out = self.model(image_semantic_onehot)
+        return out
