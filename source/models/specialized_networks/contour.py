21548d57e6bf4a1112c36d08aba6c610e959cef9
oliver_lemke <lemke.oliver@icloud.com>
2023-06-04 16:02:38 +0200

    Commented specialized networks

--- source/models/specialized_networks/contour.py
+++ source/models/specialized_networks/contour.py
@@ -4,7 +4,9 @@ from models.specialized_networks import model_utils
 
 
 class ContourModel(nn.Module):
-    """ """
+    """
+    Adds an additional input channel to receive semantic contour map
+    """
 
     def __init__(self, pretrained_model, get_func, set_func):
         super().__init__()
