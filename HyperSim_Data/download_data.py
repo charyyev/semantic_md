ca3beb15f71315a272d321b6cf0df3986e32c034
Ankita Ghosh <47214730+ankitaghosh9@users.noreply.github.com>
2023-06-11 12:35:24 +0200

    Adding dataset folder

---
+++ HyperSim_Data/download_data.py
@@ -0,0 +1,11 @@
+import os
+
+for i in range(0, 10):
+	os.system('./download.py --directory \'depth\' --contains frame.000'+str(i)+'.depth_meters.hdf5 --silent')
+	os.system('./download.py --directory \'image\' --contains frame.000'+str(i)+'.color.hdf5 --silent')
+	os.system('./download.py --directory \'semantic\' --contains frame.000'+str(i)+'.semantic.hdf5 --silent')
+
+for i in range(10, 33):
+	os.system('./download.py --directory \'depth\' --contains frame.00'+str(i)+'.depth_meters.hdf5 --silent')
+	os.system('./download.py --directory \'image\' --contains frame.00'+str(i)+'.color.hdf5 --silent')
+	os.system('./download.py --directory \'semantic\' --contains frame.00'+str(i)+'.semantic.hdf5 --silent')
